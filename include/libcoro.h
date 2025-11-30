35df046c6e7947411511dba1cc2a15ef34d6fc14
Keniis0712 <keniis@126.com>
2025-11-30 20:56:17 +0800

    Fix deps and include path

--- include/libcoro.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "coro/future.h"
-#include "coro/generator.h"
-#include "coro/loop.h"
-#include "coro/task.h"
---
+++ include/public/libcoro.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "../private/future.h"
+#include "../private/generator.h"
+#include "../private/loop.h"
+#include "../private/task.h"
